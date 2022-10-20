40f92898bd30c38f97a2495b3d756f12b723901b
baharbubbles <baharirmak222@gmail.com>
2022-10-20 16:51:29 +0300

    ders ödev

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,22 +1,93 @@
 var express = require('express');
 var router = express.Router();
-const anaSayfa=function(req, res, next)
-  {
-    res.render('anasayfa', { title: 'Ana Sayfa' });
-  }
-  const mekanBilgisi=function(req, res, next)
-  {
-    res.render('mekanbilgisi', { title: 'Mekan Bilgisi' });
-  }
-  const yorumEkle=function(req, res, next)
-  {
-    res.render('yorumekle', { title: 'Yorum ekle' });
-  }
+const mekanBilgisi = function (req, res) {
+  res.render('mekanbilgisi',
+    {
+      "baslik": "Mekan Bilgisi",
+      "mekanBaslik": "Antre Gurme Kitchen",
+      "mekanDetay": {
+        "ad": "Antre Gurme Kitchen",
+        "adres": "Isparta Merkez",
+        "puan": "5",
+        "imkanlar": ["Restoran"],
+        "koordinatlar": {
+          "enlem": "37.7812653",
+          "boylam": "30.5653227"
+        },
+        "saatler": [
+          {
+            "günler": "Pazartesi-Cuma",
+            "acilis": "09.00",
+            "kapanis": "23.00",
+            "kapali": false
+          },
+          {
+            "günler": "Cumartesi-Pazar",
+            "acilis": "08.00",
+            "kapanis": "00.00",
+            "kapali": false
 
-  
+          }
+        ],
+        "yorumlar": [
+          {
+            "yorumYapan": "Ahmet Işık",
+            "puan": "5",
+            "tarih": "10 Ağustos 2022",
+            "yorumMetni": "Yemekler güzel, servis hızlı."
+          },
+          {
+            "yorumYapan": "Gülin Aslan",
+            "puan": "4",
+            "tarih": "12 Mart 2022",
+            "yorumMetni": "Aile ile gidilebilecek güzel bir yer."
+          }
+        ]
 
 
-  module.exports = 
-  {
-    anaSayfa,mekanBilgisi,yorumEkle
-  }
+      }
+    }
+  );
+}
+
+const anaSayfa = function (req, res, next) {
+  res.render('anasayfa', 
+  { baslik: 'Ana Sayfa', 
+  sayfaBaslik: { slogan: 'Civardaki mekanları keşfet!', siteAd: 'MekanBul' },
+  mekanlar: [
+    {
+      "ad": "Antre Gurme Kitchen",
+      "adres": "Isparta Merkez",
+      "puan": "5",
+      "imkanlar": ["Restoran"],
+      "mesafe": "40m"
+    },
+    {
+      "ad": "Brewmood Cafe",
+      "adres": "Isparta Merkez",
+      "puan": "4",
+      "imkanlar": ["Kahve"],
+      "mesafe": "140m"
+    },
+    {
+      "ad": "HollStone Gece Kulübü",
+      "adres": "Isparta Merkez",
+      "puan": "5",
+      "imkanlar": ["Müzikli Eğlence"],
+      "mesafe": "400m"
+    }
+  ]
+
+});
+}
+const yorumEkle = function (req, res, next) {
+  res.render('yorumekle', { title: 'Yorum ekle' });
+}
+
+
+
+
+module.exports =
+{
+  anaSayfa, mekanBilgisi, yorumEkle
+}
