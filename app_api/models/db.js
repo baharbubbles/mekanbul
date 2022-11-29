6dbdec0eaf3e35ab186f6780d5327bfb86438639
baharbubbles <baharirmak222@gmail.com>
2022-11-29 22:38:57 +0300

    .

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,5 +1,5 @@
 var mongoose = require( 'mongoose' );
-var dbURI = 'mongodb+srv://mekan32:bahar@cluster0.ozn5dna.mongodb.net/?retryWrites=true&w=majority'; 
+var dbURI = 'mongodb+srv://mekan32:bahar@cluster0.ozn5dna.mongodb.net/mekanbul'; 
 mongoose.connect(dbURI);
 mongoose.connection.on("connected", function() {
     console.log(dbURI+"Adresindeki veritabanına baglanıldı!\n");
